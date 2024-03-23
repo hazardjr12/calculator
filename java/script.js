5d2daf665712fa5c3d8701fa174d85dc9e843f29
Faleye Olumide Hakeem <faleyehakeem@yahoo.com>
2024-03-23 17:35:50 -0400

    nexts

---
+++ java/script.js
@@ -0,0 +1,25 @@
+var display = document.querySelector("#display");
+function sin() {
+  display.value = Math.sin(display.value);
+}
+function cos() {
+  display.value = Math.cos(display.value);
+}
+function tan() {
+  display.value = Math.tan(display.value);
+}
+function log() {
+  display.value = Math.log(display.value);
+}
+function pow() {
+  display.value = Math.pow(display.value, 2);
+}
+function sqrt() {
+  display.value = Math.sqrt(display.value, 2);
+}
+function e() {
+  display.value = 2.71828182846;
+}
+function PI() {
+  display.value = 3.14159265359;
+}
